dd0dec17b677c517e092db75920000cca97a78e1
Dan Rollo <danrollo@gmail.com>
2019-11-08 11:09:52 -0500

    debug linter status

--- .circleci/ci-run.sh
+++ .circleci/ci-run.sh
@@ -5,8 +5,9 @@ source .venv/bin/activate
 pylint jake
 
 LINTER_STATUS=$?
+echo "LINTER_STATUS: $LINTER_STATUS"
 if [ $LINTER_STATUS -ne 0 ]; then
-  echo “failed linter status: $status”
+  echo "failed linter status: $status"
   exit 1
 fi
 

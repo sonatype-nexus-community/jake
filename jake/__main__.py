689326d71f2f45d31da07ed9619ad7f133a22f65
ajurgenson55 <ajurgenson@sonatype.com>
2020-04-17 21:58:01 -0400

    removed some code and fixed some issues

--- jake/__main__.py
+++ jake/__main__.py
@@ -36,7 +36,7 @@ class ArgRouter(object):
         description='Jake: Put your python deps in a chokehold'
     )
     self._sub_parsers = self._parser.add_subparsers(
-        help='subcommands: iq, ossi, config',
+        help='Select sub-command to run',
         dest='command'
     )
     self.__parse_jake_args()
@@ -46,7 +46,7 @@ class ArgRouter(object):
     iq_parser = self._sub_parsers.add_parser('iq')
 
     config_parser.add_argument(
-        'Config Type',
+        'type',
         help='set config type',
         choices=['iq', 'ossi'])
 
@@ -68,7 +68,6 @@ class ArgRouter(object):
     self._args = self._parser.parse_args()
 
   def __parse_jake_args(self):
-      # parser = argparse.ArgumentParser()
       self._parser.add_argument(
           '-N', '--snek',
           help='get python requirements instead',
@@ -90,15 +89,15 @@ def main():
   """jake entry point"""
   router = ArgRouter()
   args = router.get_args()
+  print(args)
   log = __setup_logger(args.verbose)
 
   if args.version:
     print(__version__)
     _exit(0)
 
-  print(args)
-  if args.command is 'config':
-    if args.config.iq:
+  if args.command == 'config':
+    if args.type == 'iq':
       config = IQConfig()
     else:
       config = Config()
@@ -138,6 +137,9 @@ def main():
   if args.command == 'ossi':
     audit = Audit()
     code = audit.audit_results(ossi_response)
+    if args.clean:
+      if oss_index.clean_cache():
+        print('Cache Cleared')
     _exit(code)
 
   # TODO: determine if joining conda and pypi purls for hybridized IQ results is feasible
@@ -148,46 +150,6 @@ def main():
 
   # if args.application:
   #    coords.join_coords(Pip().get_dependencies().get_coordinates())
-
-
-def __add_parser_args_and_return():
-  # TODO: figure out how subparsers work to make this cleaner and do input validation
-  parser = argparse.ArgumentParser()
-  parser.add_argument('run', help='run jake', choices=['ddt'])
-  parser.add_argument(
-      '-S', '--snake',
-      help='set optional jake config',
-      action='store_true')
-  parser.add_argument(
-      '-P', '--python',
-      help='set optional jake IQ Server config',
-      action='store_true')
-  parser.add_argument(
-      '-N', '--snek',
-      help='get python requirements instead',
-      action='store_true')
-  parser.add_argument(
-      '-V', '--version',
-      help='show program version and exit',
-      action='store_true')
-  parser.add_argument(
-      '-VV', '--verbose',
-      help="set verbosity level to debug",
-      action='store_true')
-  parser.add_argument(
-      '-a', '--application',
-      help='supply an IQ Server Public Application ID')
-  parser.add_argument(
-      '-s', '--stage',
-      help='specify a stage',
-      default='develop',
-      choices=['develop', 'build', 'stage-release', 'release'])
-  parser.add_argument(
-      '-C', '--clean',
-      help='wipe out jake cache',
-      action='store_true')
-
-  return parser.parse_args()
 
 def __setup_logger(verbose):
   logging.basicConfig(level=logging.NOTSET)
@@ -220,13 +182,6 @@ def __handle_iq_server(application_id, stage, response, log, config: Config):
   else:
     print(
         "All good to go! Smooth sailing for you! No policy violations reported by IQ Server")
-    _exit(0)
-
-def __get_config_from_std_in(config: Config):
-  result = config.get_config_from_std_in()
-  if result is False:
-    _exit(EX_OSERR)
-  else:
     _exit(0)
 
 if __name__ == '__main__':

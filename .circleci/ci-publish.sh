ad603aba744a385c1b7314294457d727cb0ffedd
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2020-04-23 14:39:14 -0800

    Follow a tip from my buddy @bhamail, use a shell script, and setup a venv, etc..., and see if this works without sudo

---
+++ .circleci/ci-publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+pip3 install -r requirements.txt
+
+pip3 install python-semantic-release
+
+pip3 install --upgrade setuptools twine wheel
+
+git config user.name "$GITHUB_USERNAME"
+
+git config user.email "$GITHUB_EMAIL"
+
+semantic-release publish --minor

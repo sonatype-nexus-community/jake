5d38dc4628a41c0bb17a2597baa46fb52fe8a116
Sarah Conord <sarahzconord@gmail.com>
2020-05-27 22:17:39 -0400

    implemented f strings

--- jake/audit/audit.py
+++ jake/audit/audit.py
@@ -22,111 +22,112 @@ from terminaltables import DoubleTable
 from ..types.coordinateresults import CoordinateResults
 from ..types.vulnerabilities import Vulnerabilities
 
-class Audit():
-  """ Audit does the business, it prints results from OSS Index to the standard out """
-  def __init__(self, quiet=False):
-    self._log = logging.getLogger('jake')
-    self._quiet = quiet
 
-  def audit_results(self, results: List[CoordinateResults]):
-    """
+class Audit:
+    """ Audit does the business, it prints results from OSS Index to the standard out """
+
+    def __init__(self, quiet=False):
+        self._log = logging.getLogger("jake")
+        self._quiet = quiet
+
+    def audit_results(self, results: List[CoordinateResults]):
+        """
     audit_results is the ingest point for the results from OSS Index,
     and handles control flow
     """
-    self._log.debug("Results recieved, %s total results", len(results))
+        self._log.debug("Results received, %s total results", len(results))
 
-    total_vulns = 0
-    pkg_num = 0
+        total_vulns = 0
+        pkg_num = 0
 
-    for coordinate in results:
-      pkg_num += 1
-      total_vulns += self.print_result(coordinate, pkg_num, len(results))
+        for coordinate in results:
+            pkg_num += 1
+            total_vulns += self.print_result(coordinate, pkg_num, len(results))
 
-    table_data = [
-        ["Audited Dependencies", len(results)],
-        ["Vulnerablities Found", total_vulns],
-    ]
+        table_data = [
+            ["Audited Dependencies", len(results)],
+            ["Vulnerablities Found", total_vulns],
+        ]
 
-    table_instance = DoubleTable(table_data, "Summary")
+        table_instance = DoubleTable(table_data, "Summary")
 
-    print()
+        print()
 
-    print(table_instance.table)
+        print(table_instance.table)
 
-    return total_vulns
+        return total_vulns
 
-  def print_result(self, coordinate: CoordinateResults, number, length):
-    """
+    def print_result(self, coordinate: CoordinateResults, number, length):
+        """
     print_results takes a coordinate, the index of the coordinate in a list,
     and the length, and handles printing it in different formats if a
     vulnerability exists
     """
-    if len(coordinate.get_vulnerabilities()) == 0:
-      if not self._quiet:
-        self.do_print("[{}/{}] - {} - no known vulnerabilities for this version"
-                      .format(
-                          number,
-                          length,
-                          coordinate.get_coordinates()), 0)
-      return len(coordinate.get_vulnerabilities())
+        if len(coordinate.get_vulnerabilities()) == 0:
+            if not self._quiet:
+                self.do_print(
+                    f"[{number}/{length}] - {coordinate.get_coordinates()} - no known vulnerabilities for this version",
+                    0
+                )
+            return len(coordinate.get_vulnerabilities())
+        self.do_print(
+            f"[{number}/{length}] - {coordinate.get_coordinates()} [VULNERABLE] {len(coordinate.get_vulnerabilities())} known vulnerabilities for this version",
+            coordinate.get_max_cvss_score(),
+        )
+        for vulnerability in coordinate.get_vulnerabilities():
+            self.print_vulnerability(vulnerability)
+        return len(coordinate.get_vulnerabilities())
 
-    self.do_print(("[{}/{}] - {} [VULNERABLE] {} known vulnerabilities for"
-                   "this version")
-                  .format(
-                      number,
-                      length,
-                      coordinate.get_coordinates(),
-                      len(coordinate.get_vulnerabilities())), coordinate.get_max_cvss_score())
-    for vulnerability in coordinate.get_vulnerabilities():
-      self.print_vulnerability(vulnerability)
-    return len(coordinate.get_vulnerabilities())
-
-  @classmethod
-  def print_vulnerability(cls, vulnerability: Vulnerabilities):
-    """
+    @classmethod
+    def print_vulnerability(cls, vulnerability: Vulnerabilities):
+        """
     print_vulnerability takes a vulnerability, and well, it prints it
     """
-    cvss_score = vulnerability.get_cvss_score()
-    cls.do_print("ID: {}".format(vulnerability.get_id()), cvss_score)
-    cls.do_print("Title: {}".format(vulnerability.get_title()), cvss_score)
-    cls.do_print("Description: {}".format(vulnerability.get_description()), cvss_score)
-    cls.do_print("CVSS Score: {} - {}".format(vulnerability.get_cvss_score(),
-                                              cls.get_cvss_severity(cvss_score)), cvss_score)
-    if vulnerability.get_cvss_vector() is not None:
-      cls.do_print("CVSS Vector: {}".format(vulnerability.get_cvss_vector()), cvss_score)
-    cls.do_print("CVE: {}".format(vulnerability.get_cve()), cvss_score)
-    cls.do_print("Reference: {}".format(vulnerability.get_reference()), cvss_score)
-    print("----------------------------------------------------")
+        cvss_score = vulnerability.get_cvss_score()
+        cls.do_print(f"ID: {vulnerability.get_id()}", cvss_score)
+        cls.do_print(f"Title: {vulnerability.get_title()}", cvss_score)
+        cls.do_print(f"Description: {vulnerability.get_description()}", cvss_score)
+        cls.do_print(
+            f"CVSS Score: {vulnerability.get_cvss_score()} - {cls.get_cvss_severity(cvss_score)}",
+            cvss_score,
+        )
+        if vulnerability.get_cvss_vector() is not None:
+            cls.do_print(
+                f"CVSS Vector: {vulnerability.get_cvss_vector()}", cvss_score
+            )
+        cls.do_print(f"CVE: {vulnerability.get_cve()}", cvss_score)
+        cls.do_print(f"Reference: {vulnerability.get_reference()}", cvss_score)
+        print("----------------------------------------------------")
 
-  @classmethod
-  def do_print(cls, text, cvss_score):
-    """
+    @classmethod
+    def do_print(cls, text, cvss_score):
+        """
     do_print takes text, and a cvss_score and prints it in a different text depending on
     the score
     """
-    if cvss_score == 0:
-      print(Fore.GREEN + text + Fore.RESET)
-    elif 0 < cvss_score < 4:
-      print(Fore.CYAN + text + Fore.RESET)
-    elif 4 <= cvss_score < 7:
-      print(Fore.LIGHTYELLOW_EX + text  + Fore.RESET)
-    elif 7 <= cvss_score < 9:
-      print(Fore.YELLOW + text + Fore.RESET)
-    else:
-      print(Fore.RED + text + Fore.RESET)
+        if cvss_score == 0:
+            print(Fore.GREEN + text + Fore.RESET)
+        elif 0 < cvss_score < 4:
+            print(Fore.CYAN + text + Fore.RESET)
+        elif 4 <= cvss_score < 7:
+            print(Fore.LIGHTYELLOW_EX + text + Fore.RESET)
+        elif 7 <= cvss_score < 9:
+            print(Fore.YELLOW + text + Fore.RESET)
+        else:
+            print(Fore.RED + text + Fore.RESET)
 
-  @classmethod
-  def get_cvss_severity(cls, cvss_score):
-    """
+    @classmethod
+    def get_cvss_severity(cls, cvss_score):
+        """
     get_cvss_severity takes a cvss_score and returns a human readable severity for it
     """
-    if cvss_score == 0:
-      return "None"
-    elif 0 < cvss_score < 4:
-      return "Low"
-    elif 4 <= cvss_score < 7:
-      return "Medium"
-    elif 7 <= cvss_score < 9:
-      return "High"
-    else:
-      return "Critical"
+        if cvss_score == 0:
+            return "None"
+        elif 0 < cvss_score < 4:
+            return "Low"
+        elif 4 <= cvss_score < 7:
+            return "Medium"
+        elif 7 <= cvss_score < 9:
+            return "High"
+        else:
+            return "Critical"

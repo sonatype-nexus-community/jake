a0cb93ab3e558bff3c9d34d66d51a8a05d00fa69
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2020-09-08 11:15:02 -0800

    Simplify

--- jake/iq/iq.py
+++ jake/iq/iq.py
@@ -43,6 +43,10 @@ class IQ():
     self._headers = DEFAULT_HEADERS
     self._report_url = ''
     self._policy_action = None
+    self._request = requests.Session()
+
+    if self._insecure:
+      self._request.verify = False
 
     config = IQConfig()
     if config.check_if_config_exists('.iq-server-config') is False:
@@ -63,6 +67,8 @@ class IQ():
       if self._iq_url is None:
         self._iq_url = results['Server']
 
+    self._request.auth = requests.auth.HTTPBasicAuth(self._user, self._password)
+
     self._internal_id = self.get_internal_id()
 
   def get_policy_action(self):
@@ -80,21 +86,11 @@ class IQ():
   def get_internal_id(self) -> (str):
     """gets internal application id from IQ Server using the public
     application id"""
-    if self._insecure:
-      response = requests.get(
-          '{0}/api/v2/applications?publicId={1}'.format(
-              self._iq_url,
-              self._public_application_id),
-          self._headers,
-          auth=(self._user, self._password),
-          verify=False)
-    else:
-      response = requests.get(
-          '{0}/api/v2/applications?publicId={1}'.format(
-              self._iq_url,
-              self._public_application_id),
-          self._headers,
-          auth=(self._user, self._password))
+    response = self._request.get(
+        '{0}/api/v2/applications?publicId={1}'.format(
+            self._iq_url,
+            self._public_application_id),
+        self._headers)
     if response.ok:
       res = json.loads(response.text)
       if not res['applications']:
@@ -112,25 +108,13 @@ class IQ():
     LOG.debug(sbom)
     headers = self._headers
     headers['Content-Type'] = 'application/xml'
-    if self._insecure:
-      response = requests.post(
-          '{0}/api/v2/scan/applications/{1}/sources/jake?stageId={2}'.format(
-              self._iq_url,
-              self._internal_id,
-              self._stage),
-          data=sbom,
-          headers=headers,
-          auth=(self._user, self._password),
-          verify=False)
-    else:
-      response = requests.post(
-          '{0}/api/v2/scan/applications/{1}/sources/jake?stageId={2}'.format(
-              self._iq_url,
-              self._internal_id,
-              self._stage),
-          data=sbom,
-          headers=headers,
-          auth=(self._user, self._password))
+    response = self._request.post(
+        '{0}/api/v2/scan/applications/{1}/sources/jake?stageId={2}'.format(
+            self._iq_url,
+            self._internal_id,
+            self._stage),
+        data=sbom,
+        headers=headers)
     if response.ok:
       res = json.loads(response.text)
       LOG.debug(res['statusUrl'])
@@ -140,22 +124,12 @@ class IQ():
   def poll_report(self, status_url: str):
     """polls status url once a second until it gets a 200 response
     , and times out after one minute"""
-    if self._insecure:
-      polling.poll(
-          lambda: requests.get(
-              '{0}/{1}'.format(self._iq_url, status_url),
-              auth=(self._user, self._password), verify=False).text,
-          check_success=self.__handle_response,
-          step=1,
-          timeout=60)
-    else:
-      polling.poll(
-          lambda: requests.get(
-              '{0}/{1}'.format(self._iq_url, status_url),
-              auth=(self._user, self._password)).text,
-          check_success=self.__handle_response,
-          step=1,
-          timeout=60)
+    polling.poll(
+        lambda: self._request.get(
+            '{0}/{1}'.format(self._iq_url, status_url)).text,
+        check_success=self.__handle_response,
+        step=1,
+        timeout=60)
 
   def __handle_response(self, response: str) -> (bool):
     try:

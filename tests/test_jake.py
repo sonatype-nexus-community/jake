491804e3af44506853e7c173d4f1f0b07179e759
Dan Rollo <danrollo@gmail.com>
2022-01-14 14:27:34 -0500

    try to get a test running. may also need some noob dev setup docs.

--- tests/test_jake.py
+++ tests/test_jake.py
@@ -15,4 +15,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import sys
+import unittest
+from jake import app
 
+
+class TestStringMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.cmd = app._create_cmd()
+        self.orig_args = sys.argv
+        sys.argv = [sys.argv[0]]
+
+    def tearDown(self):
+        self.cmd = None
+        sys.argv = self.orig_args
+
+    def test_app_noargs(self):
+        assert self.cmd is not None
+        self.cmd.execute()
+
+
+if __name__ == '__main__':
+    unittest.main()

a0ab7eecac96ea9dd1324117651556493247eb98
Paul Horton <phorton@sonatype.com>
2022-02-21 17:11:11 +0000

    feat: typing as per PEP-561 and other refactors (#114)
    * feat: typing of `jake` WIP
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * port of fix for #112
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * resolved a bunch of typing issues
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * ci: fixed parameter references
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * ci: fixed parameter references
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * ci: fixed mypy
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * fix: updated `ossindex-lib` to latest RC which now appears to properly resolve caching issues #100
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * defined lowest dependencies and aligned
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * defined lowest dependencies and aligned
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * defined lowest dependencies and aligned
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * defined lowest dependencies and aligned
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * WIP: JSON output updated to use JSON serialisation
    - All tests passing locally
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>

--- tests/test_jake.py
+++ tests/test_jake.py
@@ -15,38 +15,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import subprocess
+from unittest import TestCase
 
-import sys
-import unittest
-
-import pytest as pytest
-from unittest.mock import patch
-
-from jake import app
+from jake.command import jake_version
 
 
-class TestStringMethods(unittest.TestCase):
+class TestJakeCmd(TestCase):
 
-    def setUp(self):
-        self.cmd = app._create_cmd()
+    def test_jake_no_args(self) -> None:
+        output = subprocess.check_output('jake', shell=True)
+        self.assertTrue('show this help message and exit' in output.decode('utf-8'))
 
-    def tearDown(self):
-        self.cmd = None
+    def test_jake_help(self):
+        output = subprocess.check_output('jake -h', shell=True)
+        self.assertTrue('show this help message and exit' in output.decode('utf-8'))
 
-    def test_app_noargs(self):
-        self.cmd.execute()
-
-    def test_app_bad_arg(self):
-        test_args = ["prog", "bad-arg-name"]
-        with patch.object(sys, 'argv', test_args):
-            with pytest.raises(SystemExit) as pytest_wrapped_e:
-                # force re-read of args
-                self.cmd.__init__()
-                self.cmd.execute()
-
-        assert pytest_wrapped_e.type == SystemExit
-        assert pytest_wrapped_e.value.code == 2
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_jake_version(self):
+        output = subprocess.check_output('jake -v', shell=True)
+        self.assertEqual(f'jake {jake_version}', output.decode('utf-8').rstrip(os.linesep))
